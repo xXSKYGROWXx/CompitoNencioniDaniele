ffbb658d0d3e542c30dd0c061807f997436d6992
xXSKYGROWXx <99392382+xXSKYGROWXx@users.noreply.github.com>
2022-11-07 10:18:49 +0100

    Add files via upload

---
+++ CompitoNencioniDaniele/CompitoNencioniDaniele/demo/src/main/java/it/fi/itismeucci/App.java
@@ -0,0 +1,12 @@
+package it.fi.itismeucci;
+
+import java.io.File;
+import java.util.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
